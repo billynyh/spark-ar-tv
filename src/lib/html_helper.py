e1f000ae770a74ca1b3373f46760477877877b5b
Billy Ng <billynyh@gmail.com>
2019-08-25 22:19:51 -0700

    basic mako

--- src/lib/html_helper.py
+++ src/lib/html_helper.py
@@ -1,3 +1,5 @@
+from mako.template import Template
+
 from lib.const import *
 from lib import data_loader
 from lib import util
@@ -6,70 +8,12 @@ def get_youtube_url(id):
     return "https://youtube.com/watch?v=%s" % id
 
 HTML_BEFORE = """
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta property="og:image" content="assets/logo.png"/>
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/style.css">
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-39524040-2"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-39524040-2');
-    </script>
-
-
-    <title>Spark AR TV</title>
-
-  </head>
-  <body>
- 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.html">Spark AR TV</a>
-
-    <div class="collapse navbar-collapse" id="navbarColor01">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-      		<a class="nav-link" href="channels.html">Channels</a>
-        </li>
-        <li class="nav-item">
-      		<a class="nav-link" href="https://forum.sparkar.wiki/d/94-spark-ar-tv-unofficial-video-tutorial-collections">Suggest content</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
 
 """
 
 
 HTML_AFTER = """
 
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script>
-
-/*
-$(document).ready(function(){
-});
-*/
-
-    </script>
-  </body>
-</html>
 
 """
 
@@ -235,16 +179,19 @@ def gen_debug(video_data):
     return html
 
 def gen_channel_html(site, debug = False):
-    html = [HTML_BEFORE]
+    html = []
     if debug:
         html += gen_debug(site.video_data)
     html += gen_channel_groups(site.groups, site.video_data)
-    html.append(HTML_AFTER)
-    return '\n'.join(html)
+
+    t = Template(filename='layouts/base.html')
+    return t.render(content = '\n'.join(html))
 
 def gen_timeline_html(site):
-    html = [HTML_BEFORE]
+    html = []
     html += gen_featured(site)
     html += gen_channel_groups(site.groups_by_time, site.video_data)
-    html.append(HTML_AFTER)
-    return '\n'.join(html)
+
+    t = Template(filename='layouts/base.html')
+    return t.render(content = '\n'.join(html))
+
