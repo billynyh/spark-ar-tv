c930111524ceeb10fb0e1897c3d3cfa506668d24
Billy Ng <billynyh@gmail.com>
2019-09-08 17:15:08 -0700

    sidebar in all pages

--- src/lib/html_helper.py
+++ src/lib/html_helper.py
@@ -10,26 +10,35 @@ def get_template(filename):
     t = Template(filename="layouts/%s" % filename, lookup=lookup)
     return t
 
+def render(t, site, **kwargs):
+    return t.render(
+        site = site,
+        navs = get_navs(site),
+        **kwargs)
+
 def gen_debug_html(site, page):
     debug_text = debug_util.get_message(site)
     t = get_template('debug.html')
-    return t.render(site=site, page = page, debug_text=debug_text)
+    return render(t, site=site, page = page, debug_text=debug_text)
 
 def gen_channel_html(site, page):
     t = get_template('channels.html')
-    return t.render(site=site, page = page)
+    return render(t, site=site, page = page)
 
 def gen_facebook_html(site, page):
     t = get_template('facebook.html')
-    return t.render(site=site, page = page)
+    return render(t, site=site, page = page)
 
 def gen_timeline_html(site, page):
     t = get_template('index.html')
-    return t.render(site = site, page = page, link_to_group = True)
+    return render(t, 
+        site = site, 
+        page = page, 
+        link_to_group = True)
 
 def gen_week_html(site, page, week):
     t = get_template('week.html')
-    return t.render(
+    return render(t, 
         site = site,
         page = page,
         week = week, 
@@ -37,18 +46,16 @@ def gen_week_html(site, page, week):
  
 def gen_topic_list_html(site, page):
     t = get_template('topics.html')
-    return t.render(
+    return render(t, 
         site = site,
         page = page,
-        navs = get_navs(site),
         large_thumb = True)
     
 def gen_topic_html(site, page, topic):
     t = get_template('topic.html')
-    return t.render(
+    return render(t, 
         site = site,
         page = page,
         topic = topic,
-        navs = get_navs(site),
         large_thumb = True)
 
