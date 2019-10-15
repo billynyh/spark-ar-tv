95bc79c44764728a0731d66c4298f342b030201b
Billy Ng <billynyh@gmail.com>
2019-10-15 00:18:19 -0700

    stat

--- src/stat.py
+++ src/stat.py
@@ -11,3 +11,16 @@ for group in groups:
     print("%s: %s videos, %s channels" % (group.title, num_vid, num_channels))
 
 print("All videos: %s" % len(site.video_data))
+
+all_ids = list(site.video_data.keys())
+all_ids = sorted(all_ids, key=lambda id: (int(site.video_data[id].view_count)), reverse=True)
+
+def format(s):
+    val = int(s)
+    if val > 1000:
+        return "%dk" % (val / 1000)
+    return val
+
+for i in range(20):
+    v = site.video_data[all_ids[i]]
+    print("%s | %s | [%s](https://youtube.com/watch?v=%s)" % (format(v.view_count), v.channel_title, v.title, v.id))
