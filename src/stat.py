1251caf493a76b45229c5b23c8d502206d599a80
Billy Ng <billynyh@gmail.com>
2019-11-09 13:37:07 -0800

    gen

--- src/stat.py
+++ src/stat.py
@@ -1,26 +1,48 @@
 from lib.data_loader import *
 import config_factory
 
-config = config_factory.load(False)
-master = master_site(config)
-site = master.global_site
-groups = site.groups_by_time
-for group in groups:
-    num_vid = len(group.ids)
-    num_channels = len(set([site.video_data[id].channel_id for id in group.ids]))
-    print("%s: %s videos, %s channels" % (group.title, num_vid, num_channels))
 
-print("All videos: %s" % len(site.video_data))
+def dump_groups_stat(groups, video_data):
+    for group in groups:
+        num_vid = len(group.ids)
+        num_channels = len(set([video_data[id].channel_id for id in group.ids]))
+        print("%s: %s videos, %s channels" % (group.title, num_vid, num_channels))
 
-all_ids = list(site.video_data.keys())
-all_ids = sorted(all_ids, key=lambda id: (int(site.video_data[id].view_count)), reverse=True)
+def dump_groups_details(groups, video_data):
+    for group in groups:
+        print(group.title)
+        for x in group.ids:
+            v = video_data[x]
+            view_count = int(v.view_count)
+            if view_count > 2000:
+                print("%s // %s | %s" % (v.id, v.title, v.view_count))
+        print()
 
-def format(s):
-    val = int(s)
-    if val > 1000:
-        return "%dk" % (val / 1000)
-    return val
+def dump_top_videos(site):
+    all_ids = list(site.video_data.keys())
+    all_ids = sorted(all_ids, key=lambda id: (int(site.video_data[id].view_count)), reverse=True)
 
-for i in range(20):
-    v = site.video_data[all_ids[i]]
-    print("%s | %s | [%s](https://youtube.com/watch?v=%s)" % (format(v.view_count), v.channel_title, v.title, v.id))
+    def format(s):
+        val = int(s)
+        if val > 1000:
+            return "%dk" % (val / 1000)
+        return val
+
+    for i in range(20):
+        v = site.video_data[all_ids[i]]
+        print("%s | %s | [%s](https://youtube.com/watch?v=%s)" % (format(v.view_count), v.channel_title, v.title, v.id))
+
+
+def main():
+    config = config_factory.load(False)
+    master = master_site(config)
+    site = master.global_site
+    groups = site.groups_by_time
+    dump_groups_stat(groups, site.video_data)
+    dump_groups_details(groups[1:6], site.video_data)
+
+    print("All videos: %s" % len(site.video_data))
+
+    dump_top_videos(site)
+
+main()
