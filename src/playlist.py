b05ae5a33ceef9d461798580ca49852694f1f316
Billy Ng <billynyh@gmail.com>
2019-11-23 15:13:34 -0800

    minor update in playlist code

--- src/playlist.py
+++ src/playlist.py
@@ -3,6 +3,14 @@ from lib.api import PlaylistApi
 from lib.data_loader import *
 import config_factory
 
+def gen_playlist_from_group(api, g):
+    playlist_id = api.create_playlist("Spark AR TV - %s" % g.title, g.title)
+
+    i = 0
+    for id in g.ids:
+        api.add_video_to_playlist(playlist_id, i, id)
+        i += 1
+
 def main():
     config = config_factory.load(False)
     master = master_site(config)
@@ -12,14 +20,11 @@ def main():
     api = PlaylistApi()
     api.auth()
 
-    for g in groups[3:10]:
+    st = 6
+    ed = st + 1
+    for g in groups[st:ed]:
         print(g.title)
-        playlist_id = api.create_playlist("Spark AR TV - %s" % g.title, g.title)
-
-        i = 0
-        for id in g.ids:
-            api.add_video_to_playlist(playlist_id, i, id)
-            i += 1
+        gen_playlist_from_group(api, g)
         
 
 
