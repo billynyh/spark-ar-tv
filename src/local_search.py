bded01fe42ae983ef991ed56c8809c80a08cabd8
Billy Ng <billynyh@gmail.com>
2020-01-08 21:32:02 -0800

    gen

--- src/local_search.py
+++ src/local_search.py
@@ -3,33 +3,40 @@ import config_factory
 import site_config
 from lib.data_loader import *
 
-def main(keyword, debug):
-    config = config_factory.load(False)
-    master = master_site(config)
-    video_data = master.global_site.video_data
-
-    print("\nLocal search: %s" % keyword)
+def search(ids, video_data, keyword, debug, search_tags):
     has_result = False
-    for id,video in video_data.items():
+    for id in ids:
+        video = video_data[id]
         match = False
         if keyword in video.title.lower():
             match = True
             if debug:
                 print("- title: %s" % video.title)
-        for tag in video.tags:
-            if keyword in tag.lower():
-                match = True
-                if debug:
-                    print("- tag: %s" % tag)
+        if search_tags:
+            for tag in video.metadata:
+                if keyword in tag.lower():
+                    match = True
+                    if debug:
+                        print("- tag: %s" % tag)
         if match:
             print("%s // %s" % (video.id, video.title))
             has_result = True
-    if not has_result:
-        print("No match")
+
+def main(keyword, debug, search_tags = False):
+    config = config_factory.load(False)
+    master = master_site(config)
+    video_data = master.global_site.video_data
+
+    print("\nLocal search: %s" % keyword)
+    for g in master.global_site.groups_by_time:
+        print("# %s" % g.title)
+        search(g.ids, video_data, keyword, debug, search_tags)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Local search')
     parser.add_argument('keyword')
     parser.add_argument('--debug', default=False, action='store_true')
+    parser.add_argument('--tags', default=False, action='store_true')
     args = parser.parse_args()
-    main(args.keyword.lower(), debug = args.debug)
+    main(args.keyword.lower(), debug = args.debug, search_tags = args.tags)
