d5e1675b695fddd6c2dad3945a3a84c95da7483a
Billy Ng <billynyh@gmail.com>
2020-04-26 21:26:30 -0700

    gen

---
+++ src/recent.py
@@ -0,0 +1,12 @@
+import config_factory
+from lib.data_loader import *
+
+def main():
+    config = config_factory.load()
+    master = master_site(config, merge_small_groups = False)
+    site = master.global_site
+    ids = [id for g in site.groups_by_day[:2] for id in g.ids]
+    print('\n'.join(ids))
+
+if __name__=="__main__":
+    main()
