b3964b9127c5438c40e5ad219210e34abd805b7b
Billy Ng <billynyh@gmail.com>
2020-05-04 00:30:45 -0700

    gen

--- src/thumbnail.py
+++ src/thumbnail.py
@@ -103,13 +103,13 @@ def main_topics():
 def main():
     master = master_site(config)
     site = master.global_site
-    ids = 'c38pVnFyr5w FbEQj1ip7_4 T273zL-t3xE Yumu3aqeRbI'
+    ids = 'fdDGSsakOFI uT-ZzkHdKFM jPrzLb-hx5Y X4z2op7b0l8'
     ids = ids.split()
 
     video_data = {id:site.video_data[id] for id in ids}
     download_all(video_data)
 
-    generate_custom_week_thumbnails(site, ids, 'week-2020-04-20')
+    generate_custom_week_thumbnails(site, ids, 'week-2020-04-27')
     #generate_channel_thumbnails(site)
 
 def main_ping():
