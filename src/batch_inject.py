e71e0cde498fbff9f11b32b610b6c90f5be2b974
Billy Ng <billynyh@gmail.com>
2020-06-21 19:55:34 -0700

    batch inject script

---
+++ src/batch_inject.py
@@ -0,0 +1,70 @@
+import config_factory
+import cleanup
+
+from lib.model import MasterSite, Site, Group, ChannelList
+from lib import util
+from lib.data_loader import *
+
+RAW = """
+# zh
+https://www.youtube.com/watch?v=B5zursnGW0k
+
+# iw
+/watch?v=9G1c0GWcqnw //איך להכין פילטר לאינסטגרם - איפור פנים (make up)
+/watch?v=cCfV1z5dW4Q //איך להכין פילטר לאינסטגרם - קעקוע פנים
+/watch?v=sFuiO1rV-Ow //איך להכין פילטר לאינסטגרם ? סרטון היכרות עם SPARK AR
+
+# en
+https://www.youtube.com/watch?v=ryTTzR3__zE
+https://www.youtube.com/watch?v=T-D6zOX_pTY
+
+# pt
+https://www.youtube.com/watch?v=ATQsCz0t_DY
+https://www.youtube.com/watch?v=6fiG-IzoePo
+
+# id
+https://www.youtube.com/watch?v=o8-wPukcHiI
+"""
+
+def parse(raw):
+    group_map = {}
+    current_group = None
+    for s in raw.split("\n"):
+        s = s.strip()
+        if not s:
+            continue
+        if s.startswith("#"):
+            if current_group:
+                group_map[current_group.title] = current_group
+            current_group = Group(title = s[1:].strip(), ids = [])
+            continue
+
+        current_group.ids.append(util.extract_youtube_id(s))
+    if current_group: 
+        group_map[current_group.title] = current_group
+    
+    return group_map
+
+def main():
+    util.prepare_cache()
+    config = config_factory.load()
+    master = master_site(config, merge_small_groups = False)
+    new_data = parse(RAW)
+    for (lang, site) in master.lang_sites.items():
+        if new_data.get(lang, None):
+            groups = site.groups + [new_data[lang]]
+
+            lines = []
+            for group in groups:
+                lines.append("# %s" % group.title)
+                for id in group.ids:
+                    lines.append("%s" % id)
+                lines.append("")
+            with open("data/%s/data.txt" % site.lang, "w") as f:
+                f.write('\n'.join(lines))
+                print("Updated %s" % f.name)
+    
+    cleanup.main()
+
+if __name__=="__main__":
+    main()
